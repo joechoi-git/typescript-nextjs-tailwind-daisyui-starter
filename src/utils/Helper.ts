934cb71b7d81dd1c23c81d7fc4516523dbd64ca8
Joe Choi <joechoi.newyork@gmail.com>
2024-06-11 12:19:08 -0400

    Tanstack table (#9)
    Tanstack table

---
+++ src/utils/Helper.ts
@@ -0,0 +1,7 @@
+export const getRandom = (min: number, max: number): number => {
+    const floatRandom = Math.random();
+    const difference = max - min;
+    const random = Math.round(difference * floatRandom);
+    const randomWithinRange = random + min;
+    return randomWithinRange;
+};
